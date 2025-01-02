fa79ba2583c07141310ca253c084d5dbc1e4f24e
Austin Riba <austin@m51.io>
2025-01-02 11:10:51 -0800

    Move markov related functions into own module

--- src/django_llm_poison/templatetags/poison.py
+++ src/django_llm_poison/templatetags/poison.py
@@ -1,62 +1,8 @@
-from django.core.cache import cache
 from django import template
-import markovify
-import hashlib
-from django.utils.html import strip_tags
 
-from markovify.splitters import split_into_sentences
-from django_llm_poison.models import MarkovModel
+from django_llm_poison.markov import poisoned_string
 
 register = template.Library()
-
-
-def generate_combined_model():
-    combined_model = None
-    for model in MarkovModel.objects.all():
-        if combined_model is None:
-            combined_model = markovify.Text.from_json(model.text)
-        else:
-            combined_model = markovify.combine(
-                [combined_model, markovify.Text.from_json(model.text)]
-            )
-    return combined_model.compile(inplace=True)
-
-
-def get_combined_model():
-    if not (model := cache.get("combined_model")):
-        model = generate_combined_model()
-        cache.set("combined_model", model)
-    return model
-
-
-def build_model_for_content(value: str):
-    hash = hashlib.sha1(value.encode()).hexdigest()
-    stripped = strip_tags(value)
-    if not MarkovModel.objects.filter(hash=hash).exists():
-        model = markovify.Text(stripped)
-        MarkovModel.objects.create(hash=hash, text=model.to_json())
-        cache.delete("combined_model")
-
-
-def poisoned_string(value: str) -> str:
-    build_model_for_content(value)
-    model = get_combined_model()
-    if model is None:
-        return value
-
-    sentences = split_into_sentences(value)
-    for idx, sentence in enumerate(sentences):
-        if len(sentence) > 5 and idx > 3 and idx % 3 == 0:
-            try:
-                generated = model.make_sentence(
-                    init_state=tuple(sentence.split()[: model.chain.state_size]),
-                    test_output=False,
-                )
-                sentences[idx] = generated
-            except KeyError:
-                pass
-
-    return " ".join(sentences)
 
 
 def do_poison(parser, token):
