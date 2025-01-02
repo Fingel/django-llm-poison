11efc73f4cd7d56dfecb3243cb8f1d2a61898548
Austin Riba <austin@m51.io>
2025-01-01 23:13:06 -0800

    Basic filter to introduce some nonsense

--- src/django_llm_poison/models.py
+++ src/django_llm_poison/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class MarkovModel(models.Model):
+    hash = models.CharField(max_length=255, primary_key=True)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.hash
