8e4f059bd0c6d65821e090a5231ad285da41bf2a
Austin Riba <austin@m51.io>
2025-01-03 14:57:41 -0800

    Only serve poisoned content to ai user agents

--- src/django_llm_poison/templatetags/poison.py
+++ src/django_llm_poison/templatetags/poison.py
@@ -1,8 +1,22 @@
 from django import template
-
+import logging
 from django_llm_poison.markov import poisoned_string
+from django_llm_poison.agents import AGENTS
 
 register = template.Library()
+logger = logging.getLogger(__name__)
+
+
+def request_from_bot(request) -> bool:
+    if request.GET.get("poison"):
+        return True
+    user_agent = request.META.get("HTTP_USER_AGENT", "")
+    logger.info("Serving poisoned content to %s", user_agent)
+    for agent in AGENTS:
+        if agent in user_agent:
+            return True
+
+    return False
 
 
 def do_poison(parser, token):
@@ -17,8 +31,10 @@ class PoisonNode(template.Node):
 
     def render(self, context):
         output = self.nodelist.render(context)
-        poisoned = poisoned_string(output)
-        return poisoned
+        if request_from_bot(context["request"]):
+            return poisoned_string(output)
+        else:
+            return output
 
 
 register.tag("poison", do_poison)
